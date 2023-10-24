659cc61890121f8cad939d414a44a8e45dfb5f34
Ann Leker Wallace <ann.leker.wallace@gmail.com>
2023-10-23 21:45:50 -0700

    fourth commit - update to hangman and blackjack split working

--- commands/blackjack.py
+++ commands/blackjack.py
@@ -15,7 +15,7 @@ def register_blackjack_command(client):
         message = await interaction.followup.send('Starting game...')
 
         # Creating and shuffling the deck
-        deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'] * 4
+        deck: deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'] * 4
         random.shuffle(deck)
 
         # Function to convert face cards and aces to their numerical values
@@ -38,17 +38,12 @@ def register_blackjack_command(client):
 
         # Initial hands for player and dealer
         player_hand = [deck.pop(), deck.pop()]
+        player_hand_2 = []
         dealer_hand = [deck.pop(), deck.pop()]
 
-        # Check for Blackjack
-        if hand_value(player_hand) == 21:
-            embed = discord.Embed(
-                title="Blackjack",
-                description="**Blackjack, you win!**",
-                color=0xd3f5f7
-            )
-            await interaction.response.send_message(embed=embed, ephemeral=False)
-            return  # Exit early as the game is over
+        # Check if the player can split their hand
+        can_split = player_hand[0] == player_hand[1]
+        split_hand = False
 
         # Creating the initial embed with instructions
         embed = discord.Embed(
@@ -66,10 +61,12 @@ def register_blackjack_command(client):
 
         await message.add_reaction("✅")  # Reaction for "Hit"
         await message.add_reaction("❌")  # Reaction for "Stand"
+        if can_split:
+            await message.add_reaction("🪚")  # Reaction for "Split" if applicable
 
         # Defining the check for reactions
         def check(reaction, user):
-            return user == interaction.user and str(reaction.emoji) in ["✅", "❌"] and reaction.message.id == message.id
+            return user == interaction.user and str(reaction.emoji) in ["✅", "❌", "🪚"] and reaction.message.id == message.id
 
         # Loop for handling Hit or Stand reactions
         while True:
@@ -79,13 +76,69 @@ def register_blackjack_command(client):
 
             reaction, user = await client.wait_for('reaction_add', check=check)
             await message.remove_reaction(reaction, user)  # Remove the user's reaction
-            if str(reaction.emoji) == "✅":
+            if can_split:
+                await message.remove_reaction("🪚", client.user)
+
+            if str(reaction.emoji) == "🪚": # player wants to split
+                player_hand_2 = [ player_hand.pop(1) ]
+                split_hand = True
+                description =  f"Click ✅ to hit or ❌ to stand\n\n"
+                description += f"Your hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
+                description += f"Your second hand: {', '.join(player_hand_2)}\nTotal: {hand_value(player_hand_2)}\n\n"
+                description += f"Dealer's face-up card: {dealer_hand[0]}"
+                embed.description = description
+                await message.edit(embed=embed)
+
+            elif str(reaction.emoji) == "✅":
                 player_hand.append(deck.pop())
-                description = (f"Click ✅ to hit or ❌ to stand\n\n"
-                            f"Your hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
-                            f"Dealer's face-up card: {dealer_hand[0]}")
                 if hand_value(player_hand) > 21:
-                    description = (f"Your hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
+                    description =  f"Your hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
+                    if split_hand:
+                        description += f"Your second hand: {', '.join(player_hand_2)}\nTotal: {hand_value(player_hand_2)}\n\n"
+                    description += f"Dealer's face-up card: {dealer_hand[0]}\n\n**You busted! Game over.**"
+
+                    embed.description = description
+                    await message.edit(embed=embed)
+                    await message.clear_reactions()  # Clear reactions here
+                    if split_hand:
+                        break
+                    return  # Exit the function if the player busts
+                else:
+                    description =  f"Click ✅ to hit or ❌ to stand\n\n"
+                    description += f"Your hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
+                    if split_hand:
+                        description += f"Your second hand: {', '.join(player_hand_2)}\nTotal: {hand_value(player_hand_2)}\n\n"
+                    description += f"Dealer's face-up card: {dealer_hand[0]}"
+                    embed.description = description
+                    await message.edit(embed=embed)
+                                      
+            else:
+                if split_hand:
+                    description = (f"**Playing the second hand** \n\nClick ✅ to hit or ❌ to stand\n\n"
+                                f"Your first hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
+                                f"Your second hand: {', '.join(player_hand_2)}\nTotal: {hand_value(player_hand_2)}\n\n"
+                                f"Dealer's face-up card: {dealer_hand[0]}")  
+                    embed.description = description
+                    await message.edit(embed=embed)
+
+                break  # Exit the loop if the "Stand" reaction is received
+
+        while split_hand:
+            # Check if the player's hand value is 21, if so, break out of the loop
+            if hand_value(player_hand_2) == 21:
+                break
+
+            reaction, user = await client.wait_for('reaction_add', check=check)
+            await message.remove_reaction(reaction, user)  # Remove the user's reaction
+            if str(reaction.emoji) == "✅":
+                player_hand_2.append(deck.pop())
+                description = (f"**Playing the second hand** \n\nClick ✅ to hit or ❌ to stand\n\n"
+                            f"Your first hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
+                            f"Your second hand: {', '.join(player_hand_2)}\nTotal: {hand_value(player_hand_2)}\n\n"
+                            f"Dealer's face-up card: {dealer_hand[0]}")
+                if hand_value(player_hand_2) > 21:
+                    description = (f"Your first hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
+                                   f"Your second hand: {', '.join(player_hand_2)}\nTotal: {hand_value(player_hand_2)}\n\n"
                                    f"Dealer's face-up card: {dealer_hand[0]}\n\n**You busted! Game over.**")  # Updated description
                     embed.description = description
                     await message.edit(embed=embed)
@@ -102,28 +155,78 @@ def register_blackjack_command(client):
 
         # Determining the outcome of the game
         player_total = hand_value(player_hand)
+        player_total_2 = hand_value(player_hand_2)
         dealer_total = hand_value(dealer_hand)
         outcome = ""
-        if dealer_total > 21:
-            outcome = "**Dealer bust, you win!**"
-        else:
-            outcome = "**It's a tie.**" if player_total == dealer_total else "**You win!**" if player_total > dealer_total else "**You lose.**"
 
         # Update user_scores in the blackjack command
         user_id = str(interaction.user.id)
         if user_id not in user_scores:
             user_scores[user_id] = {'wins': 0, 'losses': 0}
-        if outcome == "**You win!**" or outcome == "**Dealer bust, you win!**":
-            user_scores[user_id]['wins'] += 1
-        elif outcome == "**You lose.**":
-            user_scores[user_id]['losses'] += 1
 
-        print(user_scores)  # Debug print here
-        
+        # Split hand score logic & update user_scores for leaderboard.py
+        if split_hand:
+            if dealer_total > 21:
+                outcome = "**Dealer bust, you win both hands!**"
+                user_scores[user_id]['wins'] += 2
+            elif dealer_total < player_total and dealer_total < player_total_2:
+                outcome = "**You win both hands!**"
+                user_scores[user_id]['wins'] += 2
+            elif dealer_total < player_total and dealer_total == player_total_2:
+                outcome = "**You win the first hand and tie the second.**"
+                user_scores[user_id]['wins'] += 1
+                # Could add something to document number of ties here
+            elif dealer_total < player_total and dealer_total > player_total_2:
+                outcome = "**You win the first hand and lose the second.**"
+                user_scores[user_id]['wins'] += 1
+                user_scores[user_id]['losses'] += 1
+            elif dealer_total == player_total and dealer_total < player_total_2:
+                outcome = "**You tie the first hand and win the second**"
+                user_scores[user_id]['wins'] += 1
+            elif dealer_total == player_total and dealer_total == player_total_2:
+                outcome = "**You tie both hands**"
+            elif dealer_total == player_total and dealer_total > player_total_2:
+                outcome = "**You tie the first hand and lose the second.**"
+                user_scores[user_id]['wins'] += 1
+            elif dealer_total > player_total and dealer_total > player_total_2:
+                outcome = "**You lose both hands.**"
+                user_scores[user_id]['losses'] += 2
+            elif dealer_total > player_total and dealer_total == player_total_2:
+                outcome = "**You lose the first hand and tie the second.**"
+                user_scores[user_id]['losses'] += 1
+            elif dealer_total > player_total and dealer_total < player_total_2:
+                outcome = "**You lose the first hand and win the second.**"
+                user_scores[user_id]['wins'] += 1
+                user_scores[user_id]['losses'] += 1
+            else: 
+                outcome = "**Message cryopumpkin.**"
+
+        # Not split hand score logic & update user_scores for leaderboard.py
+        else:
+            if dealer_total > 21:
+                outcome = "**Dealer bust, you win!**"
+                user_scores[user_id]['wins'] += 1
+            elif dealer_total < player_total:
+                outcome = "**You win!**"
+                user_scores[user_id]['wins'] += 1
+            elif dealer_total == player_total:
+                outcome = "**You tie.**"
+            elif dealer_total > player_total:
+                outcome = "**You lose.**"
+                user_scores[user_id]['losses'] += 1
+            else: 
+                outcome = "**Message cryopumpkin.**"
+
+        print(user_scores)  # Debug print here        
+
         # Update the game message with the outcome
-        description = (f"Your hand: {', '.join(player_hand)}\nTotal: {player_total}\n\n"
-                       f"Dealer's hand: {', '.join(dealer_hand)}\nTotal: {dealer_total}\n\n{outcome}")
+        description = f"Your hand: {', '.join(player_hand)}\nTotal: {hand_value(player_hand)}\n\n"
+        if split_hand:
+            description += f"Your second hand: {', '.join(player_hand_2)}\nTotal: {hand_value(player_hand_2)}\n\n"              
+        description += f"Dealer's hand: {', '.join(dealer_hand)}\nTotal: {dealer_total}\n\n{outcome}"
         embed.description = description  
+
+
         await message.edit(embed=embed)  # Updating the embed with the game outcome
         
         # Clear all reactions after the game has concluded
--- commands/hangman.py
+++ commands/hangman.py
@@ -1,43 +1,45 @@
-import discord
-import random
+import discord  # Import the discord.py library
+import random  # Import the random library for random number generation
 
 # List of words for Hangman
 word_list = ['python', 'java', 'javascript', 'discord', 'hangman']
 
 def get_hangman_word():
-    return random.choice(word_list)
+    # Function to get a random word from the word list
+    return random.choice(word_list)  # Return a random choice from the word list
 
 def get_display_word(word, guessed_letters):
-    display_word = ''
-    for letter in word:
-        if letter in guessed_letters:
-            display_word += letter
+    # Function to get the word to display with guessed letters revealed and unguessed letters as dashes
+    display_word = ''  # Initialize an empty string for the display word
+    for letter in word:  # Loop through each letter in the word
+        if letter in guessed_letters:  # If the letter has been guessed
+            display_word += letter  # Add the letter to the display word
         else:
-            display_word += '-'
-    return display_word
+            display_word += '-'  # Otherwise, add a dash to the display word
+    return display_word  # Return the display word
 
-async def hangman_game(ctx):
+async def hangman_game(interaction, client):
     word_to_guess = get_hangman_word()
     guessed_letters = []
     attempts_remaining = 6  # Adjust as needed
     while attempts_remaining > 0:
         display_word = get_display_word(word_to_guess, guessed_letters)
-        await ctx.send(f'Word: {display_word}  Attempts remaining: {attempts_remaining}')
-        guess = await ctx.bot.wait_for('message', check=lambda m: m.author == ctx.author)
+        await interaction.response.send_message(f'Word: {display_word}  Attempts remaining: {attempts_remaining}')
+        guess = await client.wait_for('message', check=lambda m: m.author == interaction.user and m.guild == interaction.guild)
         guess_content = guess.content.lower()
         if len(guess_content) == 1:
             guessed_letters.append(guess_content)
             if guess_content not in word_to_guess:
                 attempts_remaining -= 1
         elif guess_content == word_to_guess:
-            await ctx.send('Congratulations! You guessed the word!')
+            await interaction.followup.send('Congratulations! You guessed the word!')
             return
         else:
-            await ctx.send('Invalid guess. Please guess a single letter or the entire word.')
+            await interaction.followup.send('Invalid guess. Please guess a single letter or the entire word.')
             continue
-    await ctx.send(f'Sorry, the word was: {word_to_guess}')
+    await interaction.followup.send(f'Sorry, the word was: {word_to_guess}')
 
 def register_hangman_command(client):
     @client.tree.command()
-    async def hangman(ctx):
-        await hangman_game(ctx)
+    async def hangman(interaction):
+        await hangman_game(interaction, client)
